ba8dd3895b64bfdb6eb676338e1ba396ea28ef95
Neil <neil-lu@graduate.uwa.edu.au>
2014-11-06 14:35:22 +0800

    remove false attribute

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106052231) do
+ActiveRecord::Schema.define(version: 20141106062700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20141106052231) do
     t.datetime "updated_at"
     t.string   "password"
     t.string   "password_digest"
-    t.string   "string"
   end
 
 end
